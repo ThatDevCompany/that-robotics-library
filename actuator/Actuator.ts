de0c4540fde8d9c636fff8dcdda2a27528a98473
Steve James <steve.james@opsbase.com>
2021-08-18 14:09:43 +1000

    Bringing up to date

--- actuator/Actuator.ts
+++ actuator/Actuator.ts
@@ -1,27 +1,22 @@
-import {
-	B2Body,
-	B2Vec2,
-	B2AppliedForce,
-	B2FixtureDef
-} from 'that-box2d-library'
-import { ConnectorOutputDevice } from '../connector'
+import { B2AppliedForce, B2Body, B2Vec2 } from "that-box2d-library";
+import { ConnectorOutputDevice } from "../connector";
 
 export enum ActuatorType {
-	WHEEL
+  WHEEL,
 }
 
 export interface ActuatorConfig {
-	id: string
-	type: ActuatorType
-	name: string
-	size: B2Vec2
-	position: B2Vec2
+  id: string;
+  type: ActuatorType;
+  name: string;
+  size: B2Vec2;
+  position: B2Vec2;
 }
 
 export interface Actuator extends ConnectorOutputDevice, ActuatorConfig {
-	body: B2Body
-	input: number
-	appliedForces: Array<B2AppliedForce>
-	CreateShapes()
-	CreateFixtures(body: B2Body)
+  body: B2Body;
+  input: number;
+  appliedForces: Array<B2AppliedForce>;
+  CreateShapes();
+  CreateFixtures(body: B2Body);
 }
